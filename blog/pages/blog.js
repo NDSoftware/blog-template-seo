05f98aca3cd00de42f7e7c4117a9a264ad65141c
dhairya.bhavsar <dhairya.bhavsar@brainvire.com>
2023-02-17 00:24:10 +0530

    Pages lint changes

--- blog/pages/blog.js
+++ blog/pages/blog.js
@@ -1,31 +1,32 @@
-import { getAllFilesFrontMatter } from '@/lib/mdx'
-import siteMetadata from '@/data/siteMetadata'
-import ListLayout from '@/layouts/ListLayout'
-import { PageSEO } from '@/components/SEO'
+// import { getAllFilesFrontMatter } from '@/lib/mdx'
+// import siteMetadata from '@/data/siteMetadata'
+// import ListLayout from '@/layouts/ListLayout'
+// import { PageSEO } from '@/components/SEO'
 
-export const POSTS_PER_PAGE = 5
+// export const POSTS_PER_PAGE = 5
 
-export async function getStaticProps() {
-  const posts = await getAllFilesFrontMatter('blog')
-  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
-  const pagination = {
-    currentPage: 1,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
-
-  return { props: { initialDisplayPosts, posts, pagination } }
-}
+// export async function getStaticProps() {
+//   const posts = await getAllFilesFrontMatter('blog')
+//   const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+//   const pagination = {
+//     currentPage: 1,
+//     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+//   }
+//
+//   return { props: { initialDisplayPosts, posts, pagination } }
+// }
 
 export default function Blog({ posts, initialDisplayPosts, pagination }) {
   return (
     <>
-      <PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <ListLayout
-        posts={posts}
-        initialDisplayPosts={initialDisplayPosts}
-        pagination={pagination}
-        title="All Posts"
-      />
+      <h1>Blog List</h1>
+      {/*<PageSEO title={`Blog - ${siteMetadata.author}`} description={siteMetadata.description} />*/}
+      {/*<ListLayout*/}
+      {/*  posts={posts}*/}
+      {/*  initialDisplayPosts={initialDisplayPosts}*/}
+      {/*  pagination={pagination}*/}
+      {/*  title="All Posts"*/}
+      {/*/>*/}
     </>
   )
 }
