9cc9435eab751b8df1eda5ed6ceba8c964c32022
dhairya.bhavsar <dhairya.bhavsar@brainvire.com>
2023-03-21 12:25:24 +0530

    robots file update.

--- components/Footer.js
+++ components/Footer.js
@@ -27,21 +27,21 @@ export default function Footer() {
         <footer className="d-flex justify-content-between align-items-center my-4 flex-wrap py-3">
           <p className="col-md-4 text-muted mb-0">&copy; 2023 login Directory</p>
           <ul className="nav col-md-8 justify-content-end">
-            <li className="nav-item">
-              <Link href="/faq" className="nav-link text-muted px-2">
-                FAQs
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/terms-of-service" className="nav-link text-muted px-2">
-                Terms
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/disclaimer" className="nav-link text-muted px-2">
-                Disclaimer
-              </Link>
-            </li>
+            {/*<li className="nav-item">*/}
+            {/*  <Link href="/faq" className="nav-link text-muted px-2">*/}
+            {/*    FAQs*/}
+            {/*  </Link>*/}
+            {/*</li>*/}
+            {/*<li className="nav-item">*/}
+            {/*  <Link href="/terms-of-service" className="nav-link text-muted px-2">*/}
+            {/*    Terms*/}
+            {/*  </Link>*/}
+            {/*</li>*/}
+            {/*<li className="nav-item">*/}
+            {/*  <Link href="/disclaimer" className="nav-link text-muted px-2">*/}
+            {/*    Disclaimer*/}
+            {/*  </Link>*/}
+            {/*</li>*/}
             <li className="nav-item">
               <Link href="/privacy-policy" className="nav-link text-muted px-2">
                 Privacy Policy
